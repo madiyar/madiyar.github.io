b721f43e73b8d78812777caea4994011c045ce90
zxmad <25566308+zxmad@users.noreply.github.com>
2019-02-23 05:06:22 +0600

    Update m.js

--- m.js
+++ m.js
@@ -23,7 +23,7 @@ for(var x = 0; x < columns; x++)
 function draw() {
 	//Black BG for the canvas
 	//translucent BG to show trail
-	ctx.fillStyle = "rgba(0, 0, 0, 1)";
+	ctx.fillStyle = "rgba(0, 0, 0, 0.05)";
 	ctx.fillRect(0, 0, c.width, c.height);
 	
 	ctx.fillStyle = "#0F0"; //green text
